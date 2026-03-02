9b92f11f5aea7517093f748903c811564125b81b
jung-min <wjdals020503@naver.com>
2026-03-02 07:58:59 +0000

    [Frontend/template] add SDPA modules

---
+++ tests/test_sdpa.py
@@ -0,0 +1,84 @@
+import sys
+import math
+import torch
+import inspect
+from typing import List
+import torch.nn.functional as F
+from torch.nn.attention import SDPBackend, sdpa_kernel 
+from torch.fx.passes.graph_drawer import FxGraphDrawer
+from torch._inductor.decomposition import decompositions
+
+def test_result(name, out, cpu_out, rtol=1e-4, atol=1e-4):
+    message = f"|{name} Test Passed|"
+    if torch.allclose(out.cpu(), cpu_out, rtol=rtol, atol=atol):
+        print("-" * len(message))
+        print(message)
+        print("-" * len(message))
+    else:
+        print("custom out: ", out.cpu())
+        print("cpu out: ", cpu_out)
+        exit(1)
+
+def test_scaled_dot_product_attention(device, backends="flash"):
+    torch.manual_seed(0)
+    n_batch_list = [1, 4, 8, 16]
+    n_head_list = [1, 4, 8, 12]
+    n_token_list = [128, 256, 512, 1024]
+    head_dim_list = [32, 64, 128]
+
+    for n_batch in n_batch_list:
+        for n_head in n_head_list:
+            for n_token in n_token_list:
+                for head_dim in head_dim_list:
+                    # Inputs
+                    query = torch.rand(n_batch, n_head, n_token, head_dim, dtype=torch.float32)
+                    key = torch.rand(n_batch, n_head, n_token, head_dim, dtype=torch.float32)
+                    value = torch.rand(n_batch, n_head, n_token, head_dim, dtype=torch.float32)
+
+                    query = query.to(device=device)
+                    key = key.to(device=device)
+                    value = value.to(device=device)
+
+                    # With NPU
+                    if backends == "flash":
+                        backends = [SDPBackend.FLASH_ATTENTION]
+                    elif backends == "math":
+                        backends = [SDPBackend.MATH]
+                    elif backends == "memory_efficient":
+                        backends = [SDPBackend.EFFICIENT_ATTENTION]
+                    else:
+                        backends = [SDPBackend.FLASH_ATTENTION, SDPBackend.MATH, SDPBackend.EFFICIENT_ATTENTION]
+
+                    with sdpa_kernel(backends=backends):
+                        opt_fn = torch.compile(dynamic=False)(F.scaled_dot_product_attention)
+                        out = opt_fn(query, key, value)
+                    
+                    out = out.to(device)
+
+                    # With CPU
+                    device = torch.device('cpu')
+                    query = query.to(device=device)
+                    key = key.to(device=device)
+                    value = value.to(device=device)
+                    cpu_out = F.scaled_dot_product_attention(query, key, value, attn_mask=None, dropout_p=0.0, is_causal=False)
+
+                    name = f"SDPA(n_batch: {n_batch}, n_head: {n_head}, n_token: {n_token}, head_dim: {head_dim})"
+                    test_result(name, out, cpu_out)
+    
+    print("All tests passed!")
+
+def clear_caches():
+    import os
+    from torch._functorch._aot_autograd.autograd_cache import AOTAutogradCache
+    from torch._inductor.codecache import FxGraphCache
+    AOTAutogradCache.clear()
+    torch._dynamo.reset()
+    os.environ["TORCHINDUCTOR_CACHE"] = "0"
+    FxGraphCache.clear()
+
+if __name__ == "__main__":
+    clear_caches()
+    
+    device = torch.device('npu:0')
+    test_scaled_dot_product_attention(device, backends="flash")
+    
